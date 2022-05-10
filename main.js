bb25020e7bb4fd8a42b06d4ae487500a925f1e10
epixieme <kirstie_hayes@hotmail.com>
2022-05-10 20:56:19 +0100

    fixed bugs

--- main.js
+++ main.js
@@ -15,8 +15,7 @@
 ///
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
-
-game.gameStart();
+  game.gameStart();
 });
 
 class Game {
@@ -36,56 +35,58 @@ class Game {
     this.winner = winner;
   }
 
-
   gameStart() {
     const squareContainer = document.querySelector(".squareContainer");
     let playerStartText = document.querySelector(".playStartText");
-    playerStartText.classList.remove("hide")
-    console.log(playerStartText.classList)
+    playerStartText.classList.remove("hide");
+    console.log(playerStartText.classList);
     button.classList.add("hide");
-   
+
     squareContainer.classList.add("bounce");
     const items = document.querySelectorAll(".squares");
     let squares = Array.from(items);
-  
+
     squares.forEach((square, index) =>
-        square.addEventListener("click", 
-this.setHumanPlayer.bind(this,squares, square, index,playerStartText)));
+      square.addEventListener(
+        "click",
+        this.setHumanPlayers.bind(this, squares, square, index, playerStartText)
+      )
+    );
   }
 
-
-  setHumanPlayer(squares, square, index,playerStartText) {
-   
-  playerStartText.classList.add('hide')
-    const human = this.humanPlayer; // x
+  setHumanPlayers(squares, square, index, playerStartText) {
     if (squares[index].innerHTML !== "") return;
-    let removeTrackingNum = this.trackBoardCells.indexOf(index + 1); // starting at index 0-8 0 will pick 1 has to be +1 or the number would be 0 which doesn't exist in tracking
-    this.trackBoardCells.splice(removeTrackingNum, 1); // if taken then remove it
-    squares[index].innerHTML =
-      squares[index].innerHTML === human ? this.computer : human;
-    setTimeout(() => {
-      console.log(this.trackBoardCells.length);
-      this.setComputerPlayer(squares, human, square, index);
-    }, 500);
+    squares[index].innerHTML = this.humanPlayer;
+    playerStartText.classList.add("hide");
+    const removeTrackingNum = this.trackBoardCells.indexOf(index + 1); // starting at index 0-8 0 will pick 1 has to be +1 or the number would be 0 which doesn't exist in tracking
+    this.trackBoardCells.splice(removeTrackingNum, 1);
+      this.winnerConditions(squares,this.humanPlayer)
+    
   }
+
+  SetComputerPlayers(squares){
+
+    const random = this.randomNumGen(this.trackBoardCells);
+    const computerIndex = this.trackBoardCells[random]; //use random number as index to pick from remaining available nums in trackboardcells array. This enables us to only pick available squares
+  
+    squares[computerIndex - 1].innerHTML = this.computerPlayer;
+  
+    this.trackBoardCells.splice(random, 1);
+    this.winnerConditions(squares)
+  }
+
 
   randomNumGen(array) {
     const randomNum = Math.floor(Math.random() * array.length); // generate random number
     return randomNum;
   }
 
-  setComputerPlayer(squares, human, square, index) {
-    const computer = this.computerPlayer;
-    const random = this.randomNumGen(this.trackBoardCells);
-    const computerIndex = this.trackBoardCells[random]; //use random number as index to pick from remaining available nums in trackboardcells array. This enables us to only pick available squares
-    if (this.trackBoardCells.length > 0) {
-      squares[computerIndex - 1].innerHTML = computer;
-    }
-    this.trackBoardCells.splice(random, 1);
-    this.winnerConditions(squares, human, computer);
-  }
 
-  winnerConditions(squares, human, computer) {
+  winnerConditions(squares, player) {
+    // setTimeout(() => {
+
+    //   this.setComputerPlayer(squares); //// figure out where to call this
+    // }, 500);
     let array = squares.map((item) => item);
 
     const waysToWin = [
@@ -101,8 +102,8 @@ this.setHumanPlayer.bind(this,squares, square, index,playerStartText)));
     ];
 
     let winOrTie = document.querySelector(".winOrTie h2");
-  
-    waysToWin.forEach((_, i) => {
+
+    for (let i = 0; i < waysToWin.length; i++) {
       const winner = waysToWin[i]; //breaks down to individual arrays
       const cellOne = array[winner[0]].innerHTML;
       const cellTwo = array[winner[1]].innerHTML;
@@ -112,80 +113,76 @@ this.setHumanPlayer.bind(this,squares, square, index,playerStartText)));
         this.trackBoardCells.length >= 0 &&
         cellOne === cellTwo &&
         cellTwo === cellThree &&
-        cellOne != ""  // this is so that if all cells are empty they don't equal each other and will only equal once populated with x or o. By making it that one of the cells has to have something in it means all '' will not equal
+        cellOne != "" // this is so that if all cells are empty they don't equal each other and will only equal once populated with x or o. By making it that one of the cells has to have something in it means all '' will not equal
       ) {
-       
-        this.boardStyles(winOrTie,squares, array, winner);
-        
-       
+        this.boardStyles(winOrTie, squares, array, winner);
+
         this.winner = true;
-       
-        cellOne ==='x'?this.playerCount += 1:this.computerCount+=1;
-      
+
+        cellOne === "x" ? (this.playerCount += 1) : (this.computerCount += 1);
+
         setTimeout(() => {
-        // squares.forEach(sq=>sq.classList.add('hide'))
-       const players = cellOne==='x'?'Human':'Computer'
-        winOrTie.textContent = `${players.toUpperCase()} WINS`;
-
-      }, 1000);
-       this.updatePlayerScore(winOrTie);
-       setTimeout(() => {
-        this.boardReset(winOrTie,squares);
-      }, 900);
-      
-
-      }
+          // squares.forEach(sq=>sq.classList.add('hide'))
+          const players = cellOne === "x" ? "Human" : "Computer";
+          winOrTie.textContent = `${players.toUpperCase()} WINS`;
+        }, 1000);
+        this.updatePlayerScore(winOrTie);
+        setTimeout(() => {
+          this.boardReset(winOrTie, squares);
+        }, 900);
+        return
+      }}
 
       if (!this.winner && this.trackBoardCells.length === 0) {
         setTimeout(() => {
-        winOrTie.textContent = "IT'S A TIE";
-      }, 1000);
-        console.log('tie')
+          winOrTie.textContent = "IT'S A TIE";
+        }, 1000);
+        console.log("tie");
         // squares.forEach(sq=>sq.classList.add('hide'))
-       
-          // this.boardReset(squares);
-          setTimeout(() => {
-            this.boardReset(winOrTie,squares);
-          }, 900);
-     
-     
+
+        // this.boardReset(squares);
+        setTimeout(() => {
+          this.boardReset(winOrTie, squares);
+        }, 900);
+        return
       }
-    console.log(this.playerCount)
-      
-      
-    });
-   
+      if (player === this.humanPlayer){
+        setTimeout(() => {
+        this.SetComputerPlayers(squares)
+      }, 500);
+      }
   }
 
   updatePlayerScore(winOrTie) {
     let playerText = document.querySelector(".score-human h2");
     playerText.textContent = `Human: ${this.playerCount} | Computer: ${this.computerCount}`;
-//  this.gameOver(winOrTie)
-
+    //  this.gameOver(winOrTie)
   }
 
-  boardStyles(winOrTie,squares, array, winner) {
+  boardStyles(winOrTie, squares, array, winner) {
     const winningStyles = array.filter((item, i) =>
       i <= 2 ? (array[winner[i]].style.background = "white") : "none"
     );
-   
   }
 
-  boardReset(winOrTie,squares) {
+  boardReset(winOrTie, squares) {
+squares.forEach((square,index)=>square.removeEventListener("click",
+this.setHumanPlayers.bind(this, squares, square, index)))
+
     setTimeout(() => {
-    winOrTie.innerText = '';
-  }, 2000);
+      winOrTie.innerText = "";
+    }, 2000);
     this.winner = false;
-  
+
     console.log(this.winner);
     this.trackBoardCells = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-   
+
     squares.forEach((square, index) => {
       square.innerHTML = "";
       square.style.background = "#289d8f";
-      square.classList.remove('hide')
+      square.classList.remove("hide");
+     
     });
-   
   }
   // gameOver(winOrTie){
   //   // this.playerCount === 3 || this.computerCount === 3
@@ -194,8 +191,6 @@ this.setHumanPlayer.bind(this,squares, square, index,playerStartText)));
   //   console.log(this.playerCount)
   // }
 }
-
-
 
 let playerCount = 0;
 let computerCount = 0;
@@ -207,7 +202,9 @@ let game = new Game(
   playerCount,
   computerCount,
   false
-); //this.cells,currentPlayer,active
+);
+
+//this.cells,currentPlayer,active
 
 // let emojis = new Game(
 //   "x",
@@ -219,8 +216,8 @@ let game = new Game(
 // );
 ///know bugs
 
-//bug if everyone chooses in one line then both can win
+//bug if everyone chooses in one line then both can win - fixed
 //cannot remove event listener or stop squares being clicked on after win
 //make responsive
 //squares can be clicked on which increases score. seems to be if you click over winning cells numerous times
-
+// need - fixed
